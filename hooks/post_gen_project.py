14c4c1682deb6be010ac8c6a16775f05f54f6af7
Zach Probst <zprobst@resilientvitality.com>
2019-11-11 10:18:34 -0800

    Removing docs

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,6 @@ subprocess.call(['poetry', 'install'])
 # Create Git Repo and push
 subprocess.call(['git', 'init'])
 subprocess.call(['git', 'add', '*'])
-subprocess.call(['git', 'commit', '-m', 'Initial commit'])
+subprocess.call(['git', 'commit', '-n', '-m', 'Initial commit'])
 subprocess.call(['git', 'remote', 'add', 'origin', '{{ cookiecutter.project_empty_repo}}'])
 subprocess.call(['git', 'push', '-u', 'origin', 'master'])
